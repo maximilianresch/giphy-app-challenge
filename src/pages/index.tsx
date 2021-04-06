bbc7059944199688d45724460b585d9e22310189
maximilianresch <maximilianresch119@gmail.com>
2021-04-06 14:46:41 +0200

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+
+const Index = () => (
+  <Container height="100vh">
+    <Main>
+      
+    </Main>
+    <DarkModeSwitch />
+  </Container>
+)
+
+export default Index
