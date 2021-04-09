9b203eb9c6230a5161b7793d3f876d0f20a2a401
maximilianresch <maximilianresch119@gmail.com>
2021-04-09 09:01:32 +0200

    router and menu design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,38 +10,48 @@ import { FcHome, FcMenu, FcLike, FcStackOfPhotos } from "react-icons/fc";
 import { useRouter } from "next/router";
 
 export const Navigation: React.FC = () => {
-  const router = useRouter();
-
   return (
     <Menu>
-      <MenuButton
-        as={IconButton}
-        aria-label="Options"
-        icon={<FcMenu size="2.3em" />}
-        ml="5"
-      />
+      <MenuButton as={IconButton} icon={<FcMenu size="2.3em" />} ml="5" />
       <MenuList fontSize={["sm", "md", "lg", "xl"]}>
-        <MenuItem
-          onClick={() => router.push("/")}
-          icon={<FcHome size="25px" />}
-        >
-          Home
-        </MenuItem>
-
-        <MenuItem
-          onClick={() => router.push("/gifs")}
-          icon={<FcStackOfPhotos size="25px" />}
-        >
-          Gifs
-        </MenuItem>
-
-        <MenuItem
-          onClick={() => router.push("/favorites")}
-          icon={<FcLike size="25px" />}
-        >
-          Favorites
-        </MenuItem>
+        <Link href="/">
+          <MenuItem fontWeight="bold" icon={<FcHome size="25px" />}>
+            Home
+          </MenuItem>
+        </Link>
+        <Link href="/gifs">
+          <MenuItem fontWeight="bold" icon={<FcStackOfPhotos size="25px" />}>
+            Gifs
+          </MenuItem>
+        </Link>
+        <Link href="/favorites">
+          <MenuItem fontWeight="bold" icon={<FcLike size="25px" />}>
+            Favorites
+          </MenuItem>
+        </Link>
       </MenuList>
     </Menu>
+  );
+};
+
+const Link = ({ children, href }: any) => {
+  const router = useRouter();
+
+  const style = {
+    marginRight: 10,
+    color: router.asPath === href ? "#EF5D60" : "#8D8D92",
+    textDecoration: router.asPath === href ? "underline" : "none",
+  };
+  return (
+    <a
+      style={style}
+      href="#"
+      onClick={(e) => {
+        e.preventDefault();
+        router.push(href);
+      }}
+    >
+      {children}
+    </a>
   );
 };
